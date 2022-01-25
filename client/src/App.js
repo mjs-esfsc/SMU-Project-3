99e3dc220b4e47be1e883e2dbcade88afd770797
umnovjp <umnovjp@yahoo.com>
2022-01-24 18:49:52 -0800

    updating app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,65 +1,27 @@
-// ApolloClient:library for JS that enables you to manage data with GraphQL
-// InMemoryCache:Data store for Apollo Client
-// ApolloProvider: Wraps your React app and places Apollo Client on the context, which enables you to access it from anywhere in your component tree
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  createHttpLink,
-} from "@apollo/client";
-// setContext: tool to authenticate requests sent to backend when user logged-in
-import { setContext } from "@apollo/client/link/context";
-import React from "react";
-// importing BrowserRouter to make app a multi-page application
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+// import Navbar from './components/Navbar';
+import Header from './components/Header';
+//import Card from './components/Card';
+import Footer from './components/Footer';
 
-// Construct our main GraphQL API endpoint
-const httpLink = createHttpLink({
-  uri: "/graphql",
-});
-
-// Construct request middleware that will attach the JWT token to every request as an `authorization` header
-const authLink = setContext((_, { headers }) => {
-  // get the authentication token from local storage if it exists
-  const token = localStorage.getItem("id_token");
-  // return the headers to the context so httpLink can read them
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : "",
-    },
-  };
-});
-
-const client = new ApolloClient({
-  // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
-  link: authLink.concat(httpLink),
+const client = new ApolloClient ({
+  uri: '/graphql',
   cache: new InMemoryCache(),
 });
-
+// In our main App component, we are rendering only single instances of Header and Navbar and several instances of Card
+// I can place <Navbar /> and  <Card /> inside that <div> in return below but they are deleted for npw
 function App() {
   return (
-    <ApolloProvider client={client}>
-      <Router>
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          </header>
-        </div>
-      </Router>
+
+    <ApolloProvider client = {client}>
+      <>
+    <div>
+      <Header /> 
+      
+    </div>
+    <Footer />
+    </>
     </ApolloProvider>
   );
 }
