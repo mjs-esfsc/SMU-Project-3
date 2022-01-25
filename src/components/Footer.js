625717b3dc4ff18448c08a16caf4a2ad2af52fba
Talhah Awan <talhahawan@gmail.com>
2022-01-24 20:10:24 -0600

    took out CSS classes which were blocking antD

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ import LinkedInLogo from './assets/LinkedIn_logo_initials.png'
 
 function Footer() {
   return (
-    <footer className="footer">
+    <footer className="">
       {/* <a href="https://www.github.com/umnovjp"><img src={GitHubCat} alt="GitHub Logo" width="38.5" height="38.5" className = 'center'/></a> */}
       <p>martin's home address here protected by copyright</p>
     </footer>
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,8 +3,8 @@ import React from 'react';
 
 function Navigation({ currentPage, handlePageChange }) {
     return (
-      <span className="nav-tabs">
-        <button className="nav-item">
+      <span className="">
+        <button className="">
           <a
             href="#cart"
             onClick={() => handlePageChange('Cart')}
@@ -15,7 +15,7 @@ function Navigation({ currentPage, handlePageChange }) {
             Cart
           </a>
         </button>
-        <button className="nav-item">
+        <button className="">
           <a
             href="#signup"
             onClick={() => handlePageChange('Signup')}
@@ -25,7 +25,7 @@ function Navigation({ currentPage, handlePageChange }) {
             Signup
           </a>
         </button>
-        <button className="nav-item">
+        <button className="">
           <a
             href="#portfolio"
             onClick={() => handlePageChange('Portfolio')}
@@ -35,7 +35,7 @@ function Navigation({ currentPage, handlePageChange }) {
             Our Fleet
           </a>
         </button>
-        <button className="nav-item">
+        <button className="">
           <a
             href="#login"
             onClick={() => handlePageChange('Login')}
