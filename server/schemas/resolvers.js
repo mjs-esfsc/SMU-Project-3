209bf6c80c9df76abc7d80c7d2fdabe9233b7d81
Martin Bedoya <macowner@macs-MBP.attlocal.net>
2022-01-26 20:12:31 -0600

    Added bcrypt to user.js and resolver.js. Also installed bcrypt dependacy

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,6 +1,6 @@
 const { AuthenticationError } = require("apollo-server-express");
 const { User, Product, Order } = require("../models");
-//const { signToken } = require('../utils/auth');
+const { signToken } = require("../utils/auth");
 //const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
 
 const resolvers = {
