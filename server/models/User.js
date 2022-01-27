3050027a84179a69e061490b7f8565d06f22e8f7
Martin Bedoya <macowner@macs-MBP.attlocal.net>
2022-01-26 20:16:58 -0600

    Added bcrypt to user.js and resolver.js. Also installed bcrypt dependacy

--- server/models/User.js
+++ server/models/User.js
@@ -37,7 +37,7 @@ const userSchema = new Schema({
   },
 });
 
-// set up pre-save middleware to create password
+// set up pre-save middleware to create pass
 userSchema.pre("save", async function (next) {
   if (this.isNew || this.isModified("password")) {
     const saltRounds = 10;
