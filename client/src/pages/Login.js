6d2c1e8edc0549af7f600e494c6efddb7e98fb8c
Talhah Awan <talhahawan@gmail.com>
2022-01-29 12:00:50 -0600

    removed react helmet

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -6,6 +6,7 @@ import { useMutation } from "@apollo/client";
 import { Link } from "react-router-dom";
 import { LOGIN } from "../utils/mutations";
 import Auth from "../utils/auth";
+import ReactDOM from 'react-dom';
 
 
 function Login(props) {
