8b812eaa434ce2f82bc9adde64c3bbcc7bf499fa
Talhah Awan <talhahawan@gmail.com>
2022-01-29 11:57:10 -0600

    added form and changed offer from app to home

--- client/src/App.js
+++ client/src/App.js
@@ -66,12 +66,6 @@ function App() {
               WE GOT HORSEPOWER.
             </p>
             </div>
-            <h1 style={{ textAlign: 'center', fontSize: '30px', fontFamily: 'fantasy', color: 'tomato' }}>What We Offer</h1>
-            <Row style={{ textAlign: 'center'}}>
-              <Col span={8}><img src="https://cdn.businessyab.com/assets/uploads/a71d63722fdd96b5209c836f024a3291_-united-states-north-carolina-wake-county-raleigh-southwest-raleigh-beryl-road-5115-penske-truck-rental-919-773-9416.jpg" alt="moving-truck" style={{ height: '300px', width: '300 px' }}></img><h1>Moving Trucks</h1></Col>
-              <Col span={8}><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5e295pYKfLy_sh--4qEOuo5trRmWxqy5E9A&usqp=CAU" alt="moving-supplies" style={{ height: '300px', width: '300 px' }}></img><h1>Moving Supplies</h1></Col>
-              <Col span={8}><img src="https://rockstarpromovers.com/wp-content/uploads/2021/01/moving-labor-in-los-angeles.png" alt="moving-supplies" style={{ height: '300px', width: '300 px' }}></img><h1>Moving Labor</h1></Col>
-            </Row>
             <Switch>
               <Route exact path="/" component={Home} />
               <Route exact path="/login" component={Login} />
