1c8787bc4af9583d3fab53ce6b35420849ff8b7c
Martin Bedoya <macowner@macs-MBP.attlocal.net>
2022-01-30 23:18:20 -0600

    refactored signup.js and login.js; tested all the way thru checkout and stripe website

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,115 +1,151 @@
 //MB: This is now the only page for Login. It needed to import useMutation, LOGIN, auth from utils and JWT.
 import React, { useState } from "react";
-import { Form, Input, Button, Checkbox } from 'antd';
+import { Form, Input, Button, Checkbox } from "antd";
 import "../components/styles/Header.css";
 import { useMutation } from "@apollo/client";
 import { Link } from "react-router-dom";
 import { LOGIN } from "../utils/mutations";
 import Auth from "../utils/auth";
-import ReactDOM from 'react-dom';
-
+import ReactDOM from "react-dom";
 
 function Login(props) {
   const [formState, setFormState] = useState({ email: "", password: "" });
   const [login, { error }] = useMutation(LOGIN);
 
-  // const handleFormSubmit = async (event) => {
-  //   event.preventDefault();
-  //   try {
-  //     const mutationResponse = await login({
-  //       variables: { email: formState.email, password: formState.password },
-  //     });
-  //     const token = mutationResponse.data.login.token;
-  //     Auth.login(token);
-  //   } catch (e) {
-  //     console.log(e);
-  //   }
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const mutationResponse = await login({
+        variables: { email: formState.email, password: formState.password },
+      });
+      const token = mutationResponse.data.login.token;
+      Auth.login(token);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+  // const onFinish = (values) => {
+  //   console.log('Success:', values);
   // };
 
-  // const handleChange = (event) => {
-  //   const { name, value } = event.target;
-  //   setFormState({
-  //     ...formState,
-  //     [name]: value,
-  //   });
+  // const onFinishFailed = (errorInfo) => {
+  //   console.log('Failed:', errorInfo);
   // };
 
-    const onFinish = (values) => {
-      console.log('Success:', values);
-    };
-  
-    const onFinishFailed = (errorInfo) => {
-      console.log('Failed:', errorInfo);
-    };
-  
- 
   return (
-    <div style={{ marginRight: '400px' }}>
-    <Form
-      name="basic"
-      labelCol={{
-        span: 8,
-      }}
-      wrapperCol={{
-        span: 16,
-      }}
-      initialValues={{
-        remember: true,
-      }}
-      onFinish={onFinish}
-      onFinishFailed={onFinishFailed}
-      autoComplete="off"
-    >
-      <Form.Item
-        label="Username"
-        name="username"
-        rules={[
-          {
-            required: true,
-            message: 'Please input your username!',
-          },
-        ]}
-      >
-        <Input />
-      </Form.Item>
+    <div className="container my-1">
+      <Link to="/signup">← Go to Signup</Link>
 
-      <Form.Item
-        label="Password"
-        name="password"
-        rules={[
-          {
-            required: true,
-            message: 'Please input your password!',
-          },
-        ]}
-      >
-        <Input.Password />
-      </Form.Item>
-
-      <Form.Item
-        name="remember"
-        valuePropName="checked"
-        wrapperCol={{
-          offset: 8,
-          span: 16,
-        }}
-      >
-        <Checkbox>Remember me</Checkbox>
-      </Form.Item>
-
-      <Form.Item
-        wrapperCol={{
-          offset: 8,
-          span: 16,
-        }}
-      >
-        <Button type="danger" htmlType="submit">
-          Submit
-        </Button>
-      </Form.Item>
-    </Form>
+      <h2>Login</h2>
+      <form onSubmit={handleFormSubmit}>
+        <div className="flex-row space-between my-2">
+          <label htmlFor="email">Email address:</label>
+          <input
+            placeholder="youremail@test.com"
+            name="email"
+            type="email"
+            id="email"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="flex-row space-between my-2">
+          <label htmlFor="pwd">Password:</label>
+          <input
+            placeholder="******"
+            name="password"
+            type="password"
+            id="pwd"
+            onChange={handleChange}
+          />
+        </div>
+        {error ? (
+          <div>
+            <p className="error-text">The provided credentials are incorrect</p>
+          </div>
+        ) : null}
+        <div className="flex-row flex-end">
+          <button type="submit">Submit</button>
+        </div>
+      </form>
     </div>
-  );  
+  );
+
+  //   <div style={{ marginRight: '400px' }}>
+  //   <Form
+  //     name="basic"
+  //     labelCol={{
+  //       span: 8,
+  //     }}
+  //     wrapperCol={{
+  //       span: 16,
+  //     }}
+  //     initialValues={{
+  //       remember: true,
+  //     }}
+  //     onFinish={onFinish}
+  //     onFinishFailed={onFinishFailed}
+  //     autoComplete="off"
+  //   >
+  //     <Form.Item
+  //       label="Username"
+  //       name="username"
+  //       rules={[
+  //         {
+  //           required: true,
+  //           message: 'Please input your username!',
+  //         },
+  //       ]}
+  //     >
+  //       <Input />
+  //     </Form.Item>
+
+  //     <Form.Item
+  //       label="Password"
+  //       name="password"
+  //       rules={[
+  //         {
+  //           required: true,
+  //           message: 'Please input your password!',
+  //         },
+  //       ]}
+  //     >
+  //       <Input.Password />
+  //     </Form.Item>
+
+  //     <Form.Item
+  //       name="remember"
+  //       valuePropName="checked"
+  //       wrapperCol={{
+  //         offset: 8,
+  //         span: 16,
+  //       }}
+  //     >
+  //       <Checkbox>Remember me</Checkbox>
+  //     </Form.Item>
+
+  //     <Form.Item
+  //       wrapperCol={{
+  //         offset: 8,
+  //         span: 16,
+  //       }}
+  //     >
+  //       <Button type="danger" htmlType="submit">
+  //         Submit
+  //       </Button>
+  //     </Form.Item>
+  //   </Form>
+  //   </div>
+  // );
+
   //   <div className="container my-1">
   //     <Link to="/signup">← Go to Signup</Link>
 
@@ -146,99 +182,6 @@ function Login(props) {
   //     </form>
   //   </div>
   // );
-
 }
 
 export default Login;
-
-// THIS IS WHAT WAS PREVIOUSLY USED
-// NOTE BEFORE CHANGES; THIS NEEDS BE REDONE BECAUSE WE USE MUTATIONS, AUTH AND JWT: Here we import a helper function that will check if the email is valid
-// import { checkPassword, validateEmail } from "./helpers";
-// function Login() {
-//   // Create state variables for the fields in the form
-//   // We are also setting their initial values to an empty string
-//   const [email, setEmail] = useState("");
-//   const [userName, setUserName] = useState("");
-//   const [password, setPassword] = useState("");
-//   const [errorMessage, setErrorMessage] = useState("");
-
-//   const handleInputChange = (e) => {
-//     // Getting the value and name of the input which triggered the change
-//     const { target } = e;
-//     const inputType = target.name;
-//     const inputValue = target.value;
-
-//     // Based on the input type, we set the state of either email, username, and password
-//     if (inputType === "email") {
-//       setEmail(inputValue);
-//     } else if (inputType === "userName") {
-//       setUserName(inputValue);
-//     } else {
-//       setPassword(inputValue);
-//     }
-//   };
-
-//   const handleFormSubmit = (e) => {
-//     // Preventing the default behavior of the form submit (which is to refresh the page)
-//     e.preventDefault();
-
-//     // First we check to see if the email is not valid or if the userName is empty. If so we set an error message to be displayed on the page.
-//     if (!validateEmail(email) || !userName) {
-//       setErrorMessage("Email or username is invalid");
-//       // We want to exit out of this code block if something is wrong so that the user can correct it
-//       return;
-//       // Then we check to see if the password is not valid. If so, we set an error message regarding the password.
-//     }
-//     if (!checkPassword(password)) {
-//       setErrorMessage(
-//         `Choose a more secure password for the account: ${userName}`
-//       );
-//       return;
-//     }
-//     alert(`Hello ${userName}`);
-
-//     // If everything goes according to plan, we want to clear out the input after a successful registration.
-//     setUserName("");
-//     setPassword("");
-//     setEmail("");
-//   };
-
-//   return (
-//     <div>
-//       <p>Hello {userName}</p>
-//       <form className="form">
-//         <input
-//           value={email}
-//           name="email"
-//           onChange={handleInputChange}
-//           type="email"
-//           placeholder="email"
-//         />
-//         <input
-//           value={userName}
-//           name="userName"
-//           onChange={handleInputChange}
-//           type="text"
-//           placeholder="username"
-//         />
-//         <input
-//           value={password}
-//           name="password"
-//           onChange={handleInputChange}
-//           type="password"
-//           placeholder="Password"
-//         />
-//         <button type="button" onClick={handleFormSubmit}>
-//           Submit
-//         </button>
-//       </form>
-//       {errorMessage && (
-//         <div>
-//           <p className="error-text">{errorMessage}</p>
-//         </div>
-//       )}
-//     </div>
-//   );
-// }
-
-// export default Login;
--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,6 +1,6 @@
 // Signup is a page with its own url
 import React, { useState } from "react";
-import { Form, Input, Button, Checkbox, InputNumber } from 'antd';
+import { Form, Input, Button, Checkbox, InputNumber } from "antd";
 import { Link } from "react-router-dom";
 import { useMutation } from "@apollo/client";
 import Auth from "../utils/auth";
@@ -35,89 +35,172 @@ function Signup(props) {
     });
   };
   // on line 35 deleted <Link to="/login">← Go to Login</Link>
-  const layout = {
-    labelCol: {
-      span: 8,
-    },
-    wrapperCol: {
-      span: 16,
-    },
-  };
+  // const layout = {
+  //   labelCol: {
+  //     span: 8,
+  //   },
+  //   wrapperCol: {
+  //     span: 16,
+  //   },
+  // };
   /* eslint-disable no-template-curly-in-string */
-  
-  const validateMessages = {
-    required: '${label} is required!',
-    types: {
-      email: '${label} is not a valid email!',
-      number: '${label} is not a valid number!',
-    },
-    number: {
-      range: '${label} must be between ${min} and ${max}',
-    },
-  };
+
+  // const validateMessages = {
+  //   required: "${label} is required!",
+  //   types: {
+  //     email: "${label} is not a valid email!",
+  //     number: "${label} is not a valid number!",
+  //   },
+  //   number: {
+  //     range: "${label} must be between ${min} and ${max}",
+  //   },
+  // };
   /* eslint-enable no-template-curly-in-string */
-  
-    const onFinish = (values) => {
-      console.log(values);
-    };
-  
-    return (
-      <div style={{ marginRight: '400px' }}>
-      <Form {...layout} name="nest-messages" onFinish={onFinish} validateMessages={validateMessages}>
-        <Form.Item
-          name={['user', 'name']}
-          label="Name"
-          rules={[
-            {
-              required: true,
-            },
-          ]}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item
-          name={['user', 'email']}
-          label="Email"
-          rules={[
-            {
-              type: 'email',
-            },
-          ]}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item
-          name={['user', 'phone']}
-          label="Phone"
-          rules={[
-            {
-              type: 'number',
-              min: 0,
-              max: 99,
-            },
-          ]}
-        >
-          <InputNumber />
-        </Form.Item>
-        <Form.Item name={['user', 'address']} label="Address">
-          <Input />
-        </Form.Item>
-        <Form.Item name={['user', 'password']} label="Password">
-          <Input.TextArea />
-        </Form.Item>
-        <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
-          <Button type="danger" htmlType="submit">
-            Submit
-          </Button>
-        </Form.Item>
-      </Form>
-      </div>
-    );
-  
+
+  // const onFinish = (values) => {
+  //   console.log(values);
+  // };
+
+  return (
+    <div className="container my-1">
+      <Link to="/login">← Go to Login</Link>
+
+      <h2>Signup</h2>
+      <form onSubmit={handleFormSubmit}>
+        <div className="flex-row space-between my-2">
+          <label htmlFor="firstName">First Name:</label>
+          <input
+            placeholder="First"
+            name="firstName"
+            type="firstName"
+            id="firstName"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="flex-row space-between my-2">
+          <label htmlFor="lastName">Last Name:</label>
+          <input
+            placeholder="Last"
+            name="lastName"
+            type="lastName"
+            id="lastName"
+            onChange={handleChange}
+          />
+        </div>
+
+        <div className="flex-row space-between my-2">
+          <label htmlFor="address">Address:</label>
+          <input
+            placeholder="Address"
+            name="address"
+            type="address"
+            id="address"
+            onChange={handleChange}
+          />
+        </div>
+
+        <div className="flex-row space-between my-2">
+          <label htmlFor="phone">Phone:</label>
+          <input
+            placeholder="Phone"
+            name="phone"
+            type="number"
+            id="phone"
+            onChange={handleChange}
+          />
+        </div>
+
+        <div className="flex-row space-between my-2">
+          <label htmlFor="email">Email:</label>
+          <input
+            placeholder="youremail@test.com"
+            name="email"
+            type="email"
+            id="email"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="flex-row space-between my-2">
+          <label htmlFor="pwd">Password:</label>
+          <input
+            placeholder="******"
+            name="password"
+            type="password"
+            id="pwd"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="flex-row flex-end">
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default Signup;
 
+//   return (
+//     <div style={{ marginRight: "400px" }}>
+//       <Form
+//         {...layout}
+//         name="nest-messages"
+//         onSubmit={handleFormSubmit}
+//         onFinish={onFinish}
+//         validateMessages={validateMessages}
+//       >
+//         <Form.Item
+//           name={["user", "name"]}
+//           label="Name"
+//           rules={[
+//             {
+//               required: true,
+//             },
+//           ]}
+//         >
+//           <Input />
+//         </Form.Item>
+//         <Form.Item
+//           name={["user", "email"]}
+//           label="Email"
+//           rules={[
+//             {
+//               type: "email",
+//             },
+//           ]}
+//         >
+//           <Input />
+//         </Form.Item>
+//         <Form.Item
+//           name={["user", "phone"]}
+//           label="Phone"
+//           // rules={[
+//           //   {
+//           //     type: 'number',
+//           //     min: 0,
+//           //     max: 99,
+//           //   },
+//           // ]}
+//         >
+//           <InputNumber />
+//         </Form.Item>
+//         <Form.Item name={["user", "address"]} label="Address">
+//           <Input />
+//         </Form.Item>
+//         <Form.Item name={["user", "password"]} label="Password">
+//           <Input.TextArea />
+//         </Form.Item>
+//         <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
+//           <Button type="danger" htmlType="submit">
+//             Submit
+//           </Button>
+//         </Form.Item>
+//       </Form>
+//     </div>
+//   );
+// }
+
+// export default Signup;
 
 //     <div className="container my-1">
 //       <Link to="/login">← Go to Login</Link>
@@ -194,4 +277,3 @@ export default Signup;
 //     </div>
 //   );
 // }
-
